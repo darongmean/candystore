ad30dd85d64c82769f13cf717b933c1fc6c96bbf
Darong Mean <contact.darong@gmail.com>
2022-05-14 22:29:06 +0700

    f Add basic structure

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"candystore/favorite/snack"
+)
+
+func main() {
+	snack.Hello()
+}
---
+++ snack/snack_test.go
@@ -0,0 +1,7 @@
+package snack
+
+import "testing"
+
+func TestRead(t *testing.T) {
+
+}
